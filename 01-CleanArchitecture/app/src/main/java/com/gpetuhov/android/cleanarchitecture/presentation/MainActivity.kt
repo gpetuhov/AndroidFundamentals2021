a7ad2dac67ccbf7b94c79c1017e48012e4e55294
Gennady Petukhov <g3pmail@gmail.com>
2021-06-01 13:00:56 +0300

    Change package structure

--- 01-CleanArchitecture/app/src/main/java/com/gpetuhov/android/cleanarchitecture/presentation/MainActivity.kt
+++ 01-CleanArchitecture/app/src/main/java/com/gpetuhov/android/cleanarchitecture/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gpetuhov.android.cleanarchitecture
+package com.gpetuhov.android.cleanarchitecture.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gpetuhov.android.cleanarchitecture.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
