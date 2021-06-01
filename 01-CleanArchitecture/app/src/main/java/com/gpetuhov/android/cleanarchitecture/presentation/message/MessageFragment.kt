2a8b32f8613c3ce3e5131295f6195e62910125a6
Gennady Petukhov <g3pmail@gmail.com>
2021-06-01 16:19:06 +0300

    Add MessageFragment

---
+++ 01-CleanArchitecture/app/src/main/java/com/gpetuhov/android/cleanarchitecture/presentation/message/MessageFragment.kt
@@ -0,0 +1,21 @@
+package com.gpetuhov.android.cleanarchitecture.presentation.message
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gpetuhov.android.cleanarchitecture.R
+
+class MessageFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_message, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // TODO: init message text
+    }
+}
